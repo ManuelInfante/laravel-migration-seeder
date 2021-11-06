9b45842c13e58eaaeea309711fc88559e5da3930
ManuelInfante <manuel.i3.infante@gmail.com>
2021-11-06 15:16:40 +0100

    push esercizio

---
+++ database/seeds/TripsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Trip;
+use Faker\Generator as Faker;
+
+class TripsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        
+        for ($i = 0; $i < 50; $i++) {
+            $new_trip = new Trip();
+            $new_trip->code_trip = $faker->bothify('??-##-???');
+            $new_trip->destination = $faker->city();
+            $new_trip->departure_date = $faker->date();
+            $new_trip->confirmed = $faker->boolean();
+            $new_trip->price = $faker->randomFloat(2, 2000, 10000);
+            $new_trip->save();
+        }   
+    }
+}
